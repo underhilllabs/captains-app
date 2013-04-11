a8c18a6835b18a598e291b37b7b9c1596fa1b30b
Bart Lantz <bartlantz@gmail.com>
2013-04-11 13:55:17 -0600

    ordered captains by votes

--- js/app.js
+++ js/app.js
@@ -3,9 +3,9 @@ this.CaptainCtrl = function ($scope) {
     {"name": "Jean-Luc Picard", "image": "img/picard.jpg", "source": "Star Trek: TNG", "votes": 0}
     , {"name": "James Tiberius Kirk","image": "img/kirk.jpg", "source": "Star Trek: TOS", "votes": 0}
     , {"name": "Kathryn Janeway", "image": "img/janeway.jpg", "source": "Star Trek Voyager ", "votes": 0}
-    , {"name": "Hikaru Sulu", "image": "img/sulu.jpg", "source": "Star Trek Movies", "votes": 0}
-    , {"name": "Mal Reynolds","image": "img/mal.jpg", "source": "Firefly", "votes": 0}
-    , {"name": "Worf Son of Mogh","image": "img/worf.jpg", "source": "Star Trek: DSN", "votes": 0}
+    , {"name": "Hikaru Sulu", "image": "img/sulu.jpg", "source": "Star Trek Movies", "votes": 6}
+    , {"name": "Mal Reynolds","image": "img/mal.jpg", "source": "Firefly", "votes": 4}
+    , {"name": "Worf, Son of Mogh","image": "img/worf.jpg", "source": "Star Trek: DSN", "votes": 0}
     , {"name": "Benjamin Sisko","image": "img/sisko.jpg", "source": "Star Trek: DSN", "votes": 0}
     , {"name": "Han Solo","image": "img/solo.jpg", "source": "Star Wars", "votes": 0}
   ];
