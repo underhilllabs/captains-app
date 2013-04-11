c0448170026e72344776be0aa3c7debcce421859
Bart Lantz <underhilllabs@gmail.com>
2013-04-11 10:11:01 -0600

    initial commit, not working

---
+++ js/app.js
@@ -0,0 +1,13 @@
+CaptainCtlr = function ($scope) {
+  $scope.captains = [
+    {"name": "Jean-Luc Picard", "image": "img/picard.jpg", "source": "Star Trek: TNG", "votes": 0}
+    , {"name": "James Tiberius Kirk","image": "img/kirk.jpg", "source": "Star Trek: TOS", "votes": 0}
+    , {"name": "Kathryn Janeway", "image": "img/janeway.jpg", "source": "Star Trek Voyager ", "votes": 0}
+    , {"name": "Hikaru Sulu", "image": "img/sulu.jpg", "source": "Star Trek Movies", "votes": 0}
+    , {"name": "Mal Reynolds","image": "img/mal.jpg", "source": "Firefly", "votes": 0}
+    , {"name": "Worf Son of Mogh","image": "img/worf.jpg", "source": "Star Trek: DSN", "votes": 0}
+    , {"name": "Benjamin Sisko","image": "img/sisko.jpg", "source": "Star Trek: DSN", "votes": 0}
+    , {"name": "Mal Reynolds","image": "img/mal.jpg", "source": "Firefly", "votes": 0}
+  ];
+};
+
