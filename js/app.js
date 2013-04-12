73d849c2ebedb48bbdb5c2a5d8373310fc0231fa
Bart Lantz <bartlantz@gmail.com>
2013-04-11 18:40:00 -0600

    fixed sorted update

--- js/app.js
+++ js/app.js
@@ -1,17 +1,18 @@
 this.CaptainCtrl = function ($scope) {
   $scope.captains = [
-    {"name": "Jean-Luc Picard", "image": "img/picard.jpg", "source": "Star Trek: TNG", "votes": 0}
-    , {"name": "James Tiberius Kirk","image": "img/kirk.jpg", "source": "Star Trek: TOS", "votes": 0}
-    , {"name": "Kathryn Janeway", "image": "img/janeway.jpg", "source": "Star Trek Voyager ", "votes": 0}
-    , {"name": "Hikaru Sulu", "image": "img/sulu.jpg", "source": "Star Trek Movies", "votes": 6}
-    , {"name": "Mal Reynolds","image": "img/mal.jpg", "source": "Firefly", "votes": 4}
-    , {"name": "Worf, Son of Mogh","image": "img/worf.jpg", "source": "Star Trek: DSN", "votes": 0}
-    , {"name": "Benjamin Sisko","image": "img/sisko.jpg", "source": "Star Trek: DSN", "votes": 0}
-    , {"name": "Han Solo","image": "img/solo.jpg", "source": "Star Wars", "votes": 0}
+    {"name": "Jean-Luc Picard", "image": "img/picard.jpg", "source": "Star Trek: TNG", "votes": 0, index: 0}
+    , {"name": "James Tiberius Kirk","image": "img/kirk.jpg", "source": "Star Trek: TOS", "votes": 0, index: 1}
+    , {"name": "Kathryn Janeway", "image": "img/janeway.jpg", "source": "Star Trek Voyager ", "votes": 0, index: 2}
+    , {"name": "Hikaru Sulu", "image": "img/sulu.jpg", "source": "Star Trek Movies", "votes": 6, index: 3}
+    , {"name": "Mal Reynolds","image": "img/mal.jpg", "source": "Firefly", "votes": 4, index: 4}
+    , {"name": "Worf, Son of Mogh","image": "img/worf.jpg", "source": "Star Trek: DSN", "votes": 0, index: 5}
+    , {"name": "Benjamin Sisko","image": "img/sisko.jpg", "source": "Star Trek: DSN", "votes": 0, index: 6}
+    , {"name": "Han Solo","image": "img/solo.jpg", "source": "Star Wars", "votes": 0, index: 7}
   ];
 
   $scope.inc = function(idx) {
-    $scope.captains[idx].votes += 1;
+    console.log("index is " + idx + " int is " + parseInt(idx));
+    $scope.captains[parseInt(idx)].votes += 1;
   }
 };
 
